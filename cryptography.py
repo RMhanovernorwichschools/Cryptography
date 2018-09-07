f7e5fc4ace824a92e8b23454df0b24f94e17ef06
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-09-07 11:48:13 -0400

    Updated from Brython Server: 9/7/2018 11:48:12 AM

--- cryptography.py
+++ cryptography.py
@@ -13,9 +13,28 @@ associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .
 assoguide=list(zip(list(associations), range(1,len(associations)+1)))
 
 while True:
+    answer=''
     task=input('Enter e to encrypt, d to decrypt, or q to quit: ')
     if task=='q':
         break
     elif task=='e':
         string=input('Message: ')
         key=input('Key: ')
+        kterm=0
+        for x in list(string):
+            for n in range(len(assoguide)):
+                if assoguide[n][0]==x:
+                    str_n=assoguide[n][1]
+                if assoguide[n][0]==list(key)[kterm]:
+                    key_n=assoguide[n][1]
+            fin_n=str_n+key_n
+            while fin_n>85:
+                fin_n -= 85
+            kterm+=1
+            if kterm>len(key):
+                kterm=0
+            for n in range(len(assoguide)):
+                if assoguide[n][1]==fin_n:
+                    piece=assoguide[n][0]
+            answer=answer+piece
+            print(answer)
