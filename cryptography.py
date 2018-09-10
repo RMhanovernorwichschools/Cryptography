852f898fe2996a8d5fdcf450497d362469d8c2e9
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-09-10 11:32:17 -0400

    Updated from Brython Server: 9/10/2018 11:32:15 AM

--- cryptography.py
+++ cryptography.py
@@ -10,7 +10,7 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-assoguide=list(zip(list(associations), range(1,len(associations)+1)))
+assoguide=list(zip(list(associations), range(0,len(associations))))
 
 while True:
     answer=''
